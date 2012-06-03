0b7dafcb0400ae4179963df905b71971ce473eed
Masayasu Kida <msyskd@gmail.com>
2012-06-03 22:36:35 +0900

    c

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Enqy::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -67,7 +67,7 @@ Enqy::Application.configure do
 
   # Setting for devise
   config.action_mailer.default_url_options = { :host => 'enqy.herokuapp.com' }
-  config.assets.initialize_on_precompile = true
+  config.assets.initialize_on_precompile = false
 
   config.serve_static_assets = true
 
